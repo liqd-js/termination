97ad31200c7c3801d7b7e6ead645ad941ff0251b
Tomáš Korenko <radixxko@gmail.com>
2024-04-27 10:28:58 +0200

    Version 0.0.1

---
+++ src/termination.ts
@@ -0,0 +1,44 @@
+const HANDLERS = Symbol('HANDLERS');
+const TERMINATE = Symbol('TERMINATE');
+
+process.on( 'SIGTERM',  () => TerminationSignal[TERMINATE]( 'SIGTERM' ));
+process.on( 'SIGINT',   () => TerminationSignal[TERMINATE]( 'SIGINT' ));
+
+type TerminationHandler = ( signal: 'SIGTERM' | 'SIGINT' | 'MANUAL' ) => Promise<boolean | undefined> | boolean | undefined;
+
+export default class TerminationSignal
+{
+    private static [HANDLERS]: Array<{ handler: TerminationHandler, priority: number }> = [];
+
+    public static on( handler: TerminationHandler, priority: number = 0 )
+    {
+        const index = this[HANDLERS].findIndex( h => h.priority < priority );
+
+        TerminationSignal[HANDLERS].splice( index === -1 ? TerminationSignal[HANDLERS].length : index, 0, { handler, priority });
+    }
+
+    public static off( handler: TerminationHandler )
+    {
+        const index = this[HANDLERS].findIndex( h => h.handler === handler );
+
+        index !== -1 && TerminationSignal[HANDLERS].splice( index, 1 );
+    }
+
+    private static async[TERMINATE]( signal: 'SIGTERM' | 'SIGINT' | 'MANUAL' )
+    {
+        for( let handler of TerminationSignal[HANDLERS] )
+        {
+            if( await handler.handler( signal ) === false )
+            {
+                return false;
+            }
+        }
+
+        process.exit( 0 );
+    }
+
+    public static async terminate( signal: 'MANUAL' )
+    {
+        return TerminationSignal[TERMINATE]( signal );
+    }
+}
